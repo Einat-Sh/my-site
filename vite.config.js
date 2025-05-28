ee335ad63a5339f61749a7a69258d2cefdd73a2a
einatSh <shovalei@post.bgu.ac.il>
2025-05-29 00:59:41 +0300

    fix plugin call for tests, setup files call + cleanup

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint';
 
-// https://vite.dev/config/
+const isTest = process.env.NODE_ENV === 'test'
+
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: isTest ? [react()] : [react(), eslint()],
   test: {
     globals: true,
     environment: 'jsdom',
     include: ['src/**/*.test.jsx'],
     exclude: ['node_modules', 'dist'],
-    setupFiles: './vitest.setup.js'
+    setupFiles: ['./vitest.setup.js']
   }
 })
